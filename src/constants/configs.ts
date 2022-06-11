be12e912bfec32f539b3dda354b7a737af82596c
mhadikz <mhkaramzadeh@gmail.com>
2022-06-11 23:00:34 +0430

    Add constant values

---
+++ src/constants/configs.ts
@@ -0,0 +1,6 @@
+export const PORT = 8080;
+export const MASTER_TOKEN = "abcd";
+
+export const ENDPOINT = "wss://ws.kraken.com";
+export const MESSAGE = `{"event":"subscribe","pair":["XBT/USD","XBT/EUR","ADA/USD"],"subscription":{"name":"ticker"}}`;
+
